fd1a25dca28a01db47144c6d1449dc87fbb520a2
Javeria Noor <jianoor053@gmail.com>
2023-12-14 15:34:40 +0500

    Initial commit

---
+++ Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesPractice
+{
+    internal class Pizza
+    {
+        public string Name { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertiesPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var pizza = new Pizza();
+            pizza.Name = "Test";
+            Console.WriteLine("I ate "+pizza.Name+" pizza");
+        }
+    }
+}
