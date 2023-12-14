7eaa47949407cafeee659b0e34fd9afcad77fe89
Javeria Noor <90166738+JiaNoor@users.noreply.github.com>
2023-12-14 16:23:51 +0500

    making accessor private

--- Pizza.cs
+++ Pizza.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,26 @@ namespace PropertiesPractice
     internal class Pizza
     {
         // 1)Property syntax
-        //public string Name { get; set; }
+        public string Name { get; set; }
 
         // 2)Setting property value to default
 
-        //public string Name { get; set; } = "Itallian";
+        /*public string Name { get; set; } = "Itallian";*/
 
         // 3)Define the storage yourself
-        string name;
-        public string Name { get => name; set { name = value.ToUpper(); }}
+        /*string name;
+        public string Name { get => name; set { name = value.ToUpper(); }}*/
 
+
+        // 4) restrict user access to change properties by making them private
+        /* by this the property can get from everywhere but can set only from it's own class*/
+        public bool OnSale {  get; private set; }
+
+        public Pizza() 
+        { 
+            OnSale = true;
+        }
+
+       
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@
             var pizza = new Pizza();
             Console.WriteLine("I ate " + pizza.Name + " pizza");
             pizza.Name = "Test";
-            Console.WriteLine("I ate "+pizza.Name+" pizza");
+
+            //pizza.OnSale = false; //The onsale cannot be accessed here b/c th set ancessor is private
+
+            Console.WriteLine("I ate " + pizza.Name + " pizza. The pizza is " + (pizza.OnSale ? "on sale" : "not on sale"));
         }
     }
 }
